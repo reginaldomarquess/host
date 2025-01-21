952bbd582db3a52f2b58cda70a4715ef903c9534
Reginaldo Marques <reginaldomarquess@outlook.com>
2025-01-20 23:33:42 -0300

    com
    commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+
+/*======== Vamos transformar o menu hambúrguer em um X==========*/
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
+
